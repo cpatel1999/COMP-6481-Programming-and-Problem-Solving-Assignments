be4992989755c78277d3e4449aca8d5b0d960c9f
charit <charit@gmail.com>
2021-10-13 15:05:23 +0530

    Code is reformatted

--- Assignment 2/src/ArrayRepeat.java
+++ Assignment 2/src/ArrayRepeat.java
@@ -2,40 +2,33 @@ import java.util.Scanner;
 
 public class ArrayRepeat {
 
-    public static void repeatElementCount(int[] arr)
-    {
-       // int previousElement = arr[0];
+    public static void repeatElementCount(int[] arr) {
+        // int previousElement = arr[0];
         int currentElement;
         int nextElement;
         int count = 1;
         int index = 0;
-        for(int i = 0; i < arr.length-1; i++)
-        {
+        for (int i = 0; i < arr.length - 1; i++) {
             currentElement = arr[i];
-            nextElement = arr[i+1];
-            if(nextElement == currentElement)
-            {
+            nextElement = arr[i + 1];
+            if (nextElement == currentElement) {
                 count++;
-            }
-            else
-            {
+            } else {
                 index = i + 1;
-                if(count != 1)
-                {
+                if (count != 1) {
                     System.out.println("Value " + currentElement + " is repeated " + count + " times stating at Index " + index);
                 }
                 count = 1;
             }
         }
     }
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         repeatElementCount(arr);
--- Assignment 2/src/ArrayRepeatUsingStack.java
+++ Assignment 2/src/ArrayRepeatUsingStack.java
@@ -3,30 +3,23 @@ import java.util.Stack;
 
 public class ArrayRepeatUsingStack {
 
-    public static void arrayRepeat(Stack<Integer> stack)
-    {
+    public static void arrayRepeat(Stack<Integer> stack) {
         int firstElement = stack.pop();
         int currentElement;
         int count = 1;
         int index = stack.size() - 1;
         //for(int i = stack.size() - 1; i > 0; i--)
         int i = stack.size() - 1;
-        while (stack.size() != 0)
-        {
+        while (stack.size() != 0) {
             currentElement = stack.pop();
-            if(currentElement == firstElement)
-            {
+            if (currentElement == firstElement) {
                 count++;
                 index = i;
-                if(stack.size() == 0)
-                {
+                if (stack.size() == 0) {
                     System.out.println("Value " + firstElement + " is repeated " + count + " times stating at Index " + index);
                 }
-            }
-            else
-            {
-                if(count != 1)
-                {
+            } else {
+                if (count != 1) {
                     System.out.println("Value " + firstElement + " is repeated " + count + " times stating at Index " + index);
                 }
                 count = 1;
@@ -36,14 +29,12 @@ public class ArrayRepeatUsingStack {
         }
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Stack<Integer> stack = new Stack<Integer>();
 
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             stack.push(sc.nextInt());
         }
         System.out.println(stack.size());

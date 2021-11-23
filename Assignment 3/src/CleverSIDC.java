9c8c37cd1aefa948df5640147b2d513e1906483d
charit <charit@gmail.com>
2021-11-23 02:25:39 -0500

    Custom LinkedHashMap class is implemented, CleverSIDC class created and setSIDCThreshold method is implemented

--- Assignment 3/src/CleverSIDC.java
+++ Assignment 3/src/CleverSIDC.java
@@ -1,2 +1,33 @@
-public class CleverSIDC {
+public class CleverSIDC<K,V> {
+
+    private LinkedHashMapCustom<K,V> map;
+    private int method_1, method_2;
+
+
+    public CleverSIDC()
+    {
+        map = new LinkedHashMapCustom<K,V>();
+    }
+
+    public void setSIDCThreshold(long size)
+    {
+        if(size > 0 && size < 100)
+        {
+            method_1 = 1;
+        }
+        else if(size >= 100 && size <= 500000)
+        {
+            method_2 = 1;
+        }
+        else
+        {
+            System.out.println("Threshold is very high!!!!");
+        }
+    }
+
+    public void generate()
+    {
+        
+    }
+
 }
